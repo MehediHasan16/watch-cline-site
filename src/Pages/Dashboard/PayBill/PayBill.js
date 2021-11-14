ebd1b48c89ff91949e9ecfe4d1f70b11883c5c21
MehediHasan16 <maher168082@gmail.com>
2021-11-15 00:42:19 +0600

    added pay style

--- src/Pages/Dashboard/PayBill/PayBill.js
+++ src/Pages/Dashboard/PayBill/PayBill.js
@@ -1,10 +1,13 @@
+import { Container } from '@mui/material';
 import React from 'react';
 
 const PayBill = () => {
     return (
         <div>
-            <h1>Pay Bill Method Comming Soon</h1>
-            <img src="https://img.freepik.com/free-vector/clients-with-magnifier-get-e-invoicing-pay-bills-online-e-invoicing-service-electronic-invoicing-e-billing-system-e-economy-tools-concept-illustration_335657-2100.jpg?size=626&ext=jpg" alt="" />
+            <Container>
+                <h1>Pay Bill Method Coming Soon</h1>
+                <img style={{ width: '100%' }} src="https://img.freepik.com/free-vector/clients-with-magnifier-get-e-invoicing-pay-bills-online-e-invoicing-service-electronic-invoicing-e-billing-system-e-economy-tools-concept-illustration_335657-2100.jpg?size=626&ext=jpg" alt="" />
+            </Container>
         </div>
     );
 };
